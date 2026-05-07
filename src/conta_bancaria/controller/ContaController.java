a100432f08ec8fe42b13f79b454f20f773102863
azapokou-art <azapokou@gmail.com>
2026-05-07 00:38:52 -0300

    implementa métodos sacar, depositar e transferir

--- src/conta_bancaria/controller/ContaController.java
+++ src/conta_bancaria/controller/ContaController.java
@@ -67,21 +67,54 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void sacar(int numero, float valor) {
-		// TODO Auto-generated method stub
+		var conta = buscarNaCollection(numero);
+		if (conta != null) {
+			
+			if (conta.sacar(valor) == true) {
+				System.out.printf("\n O saque na Conta número: %d foi efetuado com sucesso !", numero);
+			}
+		}else {
+			System.out.printf("\n A conta número: %d não foi encontrada !%n", numero);
+		}
 		
 	}
 
 	@Override
 	public void depositar(int numero, float valor) {
-		// TODO Auto-generated method stub
-		
+		var conta = buscarNaCollection(numero);
+		if (conta != null) {
+			conta.depositar(valor);
+			System.out.printf("\n O Depósito na Conta número: %d foi efetuado com sucesso !", numero);
+		}else {
+		System.out.printf("\n A conta número: %d não foi encontrada !%n", numero);
 	}
+	}
+	
 
 	@Override
 	public void transferir(int numeroOrigem, int numeroDestino, float valor) {
-		// TODO Auto-generated method stub
-		
+
+	    var contaOrigem = buscarNaCollection(numeroOrigem);
+	    var contaDestino = buscarNaCollection(numeroDestino);
+
+	    if (contaOrigem != null && contaDestino != null) {
+
+	        if (contaOrigem.sacar(valor) == true) {
+	            contaDestino.depositar(valor);
+
+	            System.out.printf(
+	                "\nA Transferência da conta: %d, para a conta: %d foi efetuada com sucesso!",
+	                numeroOrigem,
+	                numeroDestino
+	            );
+	        }
+
+	    } else {
+	        System.out.println("\nA Conta de Origem e/ou Destino não foram encontradas!");
+	    }
+
 	}
+		
 	
 	public int gerarNumero() {
 		return ++ numero;
